9cd8aedfd50a8d31e76eadbf20ac8da5ec9b7d2e
2025- <2025-0261@gmail.com>
2026-05-27 23:24:28 -0400

    Mi primer archivo desde la consola

--- obj/Debug/net10.0/prueba1.AssemblyInfo.cs
+++ obj/Debug/net10.0/prueba1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("prueba1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9237bc87efe83da939b00247e778bfd16c4451fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84f3282024ec50af9d38fd8b7288e8c023e413f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("prueba1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("prueba1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
